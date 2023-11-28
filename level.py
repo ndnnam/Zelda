ad700d2a51c326ed04543d8ed2c9fd1ea1cd9573
ndnnam <nhutnam249.gg@gmail.com>
2023-11-28 16:37:36 +0700

    Player animations

--- level.py
+++ level.py
@@ -40,6 +40,7 @@ class Level:
     def run(self):
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
+        debug(self.player.status)
 class YSortCameraGroup(pygame.sprite.Group):
     def __init__(self):
         super().__init__()
